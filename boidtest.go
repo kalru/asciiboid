30efc24de9dadb0d8cf45137f55a640278f5ac0f
kalru <47431193+kalru@users.noreply.github.com>
2022-08-14 20:37:22 +0200

    screen init

--- boidtest.go
+++ boidtest.go
@@ -1,10 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gdamore/tcell/v2"
+)
 
 func main() {
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+
+	// Initialize screen
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+	s.SetStyle(defStyle)
+	s.EnablePaste()
+	s.Clear()
+
+	quit := func() {
+		s.Fini()
+		os.Exit(0)
+	}
 
 	boid1 := Boid{0, 0, 0}
 	fmt.Printf("%s\n", boid1.to_string())
+
+	for {
+		// Update screen
+		s.Show()
+		// Poll event
+		ev := s.PollEvent()
+		// Process event
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				quit()
+			}
+		}
+	}
 
 }
